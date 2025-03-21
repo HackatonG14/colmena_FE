35390e0087f2a4be647f8391707a8a4d4c2f6826
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-21 02:33:03 +0100

    create page AdDetails and add it root

--- src/components/AdCard.jsx
+++ src/components/AdCard.jsx
@@ -1,13 +1,21 @@
-const AdCard = ({ URLimage, title, description }) => {
-    return (
-      <div className="border rounded-lg shadow-md p-4 text-center">
-        <div className="w-full h-40 bg-gray-300 flex items-center justify-center">
-          <span className="text-gray-700">{URLimage}</span>
+import { Link } from "react-router-dom";
+
+const AdCard = ({ id, imageUrl, title, description }) => {
+  return (
+    <Link to={`/ad/${id}`} className="block">
+      <div className="border rounded-lg shadow-md p-4 text-center hover:shadow-lg transition">
+        <div className="w-full h-40 bg-gray-300 flex items-center justify-center rounded-md overflow-hidden">
+          {imageUrl ? (
+            <img src={imageUrl} alt={title} className="w-full h-full object-cover" />
+          ) : (
+            <span className="text-gray-700">Imagen no disponible</span>
+          )}
         </div>
-        <h3 className="text-lg font-semibold mt-2">{title}</h3>
+        <h3 className="text-lg font-semibold mt-2 text-black">{title}</h3>
         <p className="text-gray-600">{description}</p>
       </div>
-    );
-  };
-  
-  export default AdCard;
+    </Link>
+  );
+};
+
+export default AdCard;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import CreateAd from "../pages/CreateAd";
+import AdDetails from "../pages/AdDetails";
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/createAd" element={<CreateAd />} />
+        <Route path="/ad/:id" element={<AdDetails />} /> 
       </Routes>
     </Router>
   );
